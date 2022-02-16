fddcbf3835e7f4e3ae1d04862ec8547e1e2182bf
Alejandro Perez <AlejandroPerezOlvera@outlook.com>
2022-02-16 15:03:42 -0600

    010-Creation of Votes template

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,7 @@
 """Polls Views"""
 
 # Django
+from webbrowser import get
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -24,9 +25,10 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(
-        f'You are looking the results of question {question_id}'
-    )
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 
 def vote(request, question_id):
