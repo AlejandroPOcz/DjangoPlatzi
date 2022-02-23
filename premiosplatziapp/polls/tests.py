39602f35c2357e2d03bd202a743e0e45ddd6dd9b
Alejandro Perez <AlejandroPerezOlvera@outlook.com>
2022-02-22 21:45:29 -0600

    016-Tests for Results and Detail views

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -2,6 +2,7 @@
 
 # Utils
 import datetime
+from urllib import response
 
 # Django
 from django.test import TestCase
@@ -9,7 +10,7 @@ from django.utils import timezone
 from django.urls.base import reverse
 
 # Polls
-from .models import Question
+from .models import Question, Choice
 
 
 class QuestionModelTest(TestCase):
@@ -68,6 +69,24 @@ def create_question(question_text, days):
         question_text=question_text)
 
 
+def create_choices(question, choice1=1, choice2=10):
+    """Create 2 choices with defaults 1 and 10 votes for a given question
+    
+    Args:
+        question (Question): An instance of a question
+        choice1 (int): Number of votes for choice1 (default 1)
+        choice2 (int): Number of votes for choice2 (default 1o)
+    """
+    Choice.objects.create(
+        question=question,
+        choice_text="Choice 1",
+        votes=choice1)
+    Choice.objects.create(
+        question=question,
+        choice_text="Choice 2",
+        votes=choice2)
+
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """If no question exist, an appropiate message is display"""
@@ -86,8 +105,7 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'])
 
     def test_questions_with_past_pub_date(self):
-        """Questions with date greater to timezone.now shouldn't be
-        displayed"""
+        """Questions with date in the correct range should be displayed"""
         question = create_question("Past Question", -1)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
@@ -119,3 +137,39 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(
             [past_question2, past_question1],
             response.context['latest_question_list'])
+
+
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """The detail view of a question with the pub_date in the future
+        returns a 404 error not found"""
+        future_question = create_question(
+            question_text="Future Question",
+            days=1)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """The detail view of a question with the pub_date in the past
+        displays the question's text"""
+        past_question = create_question(
+            question_text="Past Question",
+            days=-1)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
+
+
+class QuestionResultViewTests(TestCase):
+    def test_correct_display_of_votes(self):
+        question = create_question(question_text="Question", days=1)
+        create_choices(question)
+        self.assertEqual(question.choices.all().count(), 2)
+        url = reverse("polls:results", args=(question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Choice 1 -- 1 vote")
+        self.assertContains(response, "Choice 2 -- 10 votes")
